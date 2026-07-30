8df81bbe0ff1cb3c1611e467c9e52758c8ae8042
Sajed Fallah <fallah.sajed@gmail.com>
2026-07-30 18:53:01 +0330

    Phase 4.1 add mobile navigation component

---
+++ mini-app/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 flex justify-around bg-black/80 p-4 text-white">
+      <button>Home</button>
+      <button>Events</button>
+      <button>Tickets</button>
+      <button>Profile</button>
+    </nav>
+  );
+}
