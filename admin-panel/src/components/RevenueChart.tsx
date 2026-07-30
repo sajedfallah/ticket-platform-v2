997ab2a66215316ede0a22e944c273c86db34c01
Sajed Fallah <fallah.sajed@gmail.com>
2026-07-30 19:09:44 +0330

    Add revenue chart placeholder component for dashboard analytics

---
+++ admin-panel/src/components/RevenueChart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RevenueChart() {
+  return (
+    <div className="revenue-chart">
+      <h3>Revenue Overview</h3>
+      <p>Chart data will be loaded from reports API.</p>
+    </div>
+  );
+}
