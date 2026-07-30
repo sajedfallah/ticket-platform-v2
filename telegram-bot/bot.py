388e063e317cc95694f56667722d2825dde98474
Sajed Fallah <fallah.sajed@gmail.com>
2026-07-30 18:35:30 +0330

    Add Telegram Bot v2 application entrypoint

---
+++ telegram-bot/bot.py
@@ -0,0 +1,10 @@
+from config import BOT_TOKEN
+
+
+async def start_bot():
+    print("Ticket Platform Telegram Bot v2 started")
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(start_bot())
