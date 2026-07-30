bef133deda46f93576a899fc3cb58376c82effbb
Sajed Fallah <fallah.sajed@gmail.com>
2026-07-30 19:02:02 +0330

    Add create event page foundation

---
+++ admin-panel/src/pages/CreateEvent.tsx
@@ -0,0 +1,13 @@
+export default function CreateEvent() {
+  return (
+    <main>
+      <h1>Create Event</h1>
+      <form>
+        <input placeholder="Event title" />
+        <input placeholder="Venue" />
+        <input placeholder="Date" />
+        <button type="button">Save Event</button>
+      </form>
+    </main>
+  );
+}
