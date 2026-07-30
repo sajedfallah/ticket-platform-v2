e56daf90fa4f4da0f903532edb26929493e745f9
Sajed Fallah <fallah.sajed@gmail.com>
2026-07-30 19:09:51 +0330

    Add recent orders dashboard component

---
+++ admin-panel/src/components/RecentOrders.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RecentOrders() {
+  return (
+    <div className="recent-orders">
+      <h3>Recent Orders</h3>
+      <p>Orders list will be connected to orders API.</p>
+    </div>
+  );
+}
